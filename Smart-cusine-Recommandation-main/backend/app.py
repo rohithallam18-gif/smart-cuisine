d1e7e010c0a61648b3604b63e27af24be449e32d
rohithallam18-gif <rohithallam18@gmail.com>
2026-06-05 23:23:07 +0530

    Update app.py

--- Smart-cusine-Recommandation-main/backend/app.py
+++ Smart-cusine-Recommandation-main/backend/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
-from extensions import mail
-from config import Config
-from database import db
+from .extensions import mail
+from .config import Config
+from .database import db
 import os
 
 # 1. Initialize the Flask App
@@ -14,13 +14,13 @@ CORS(app)
 mail.init_app(app)
 
 # 3. Register Blueprints (from root routes folder)
-from routes.auth import auth_bp
-from routes.location import location_bp
-from routes.restaurants import restaurant_bp
-from routes.recommendations import rec_bp
-from routes.bookmarks import bookmark_bp
-from routes.reviews import review_bp
-from routes.main import main_bp
+from .routes.auth import auth_bp
+from .routes.location import location_bp
+from .routes.restaurants import restaurant_bp
+from .routes.recommendations import rec_bp
+from .routes.bookmarks import bookmark_bp
+from .routes.reviews import review_bp
+from .routes.main import main_bp
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(location_bp)
